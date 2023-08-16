bddd3f1ae972ecad211f4eaccab82f9406b6e8dd
mahtab_alam <mahtab.afridi@gmail.com>
2023-08-16 13:11:21 +0530

    Created a development branch

--- themes/contrib/newsplus_lite/js/custom.js
+++
@@ -1,7 +0,0 @@
-(function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.mtMagnificPopupFieldImage = {
-    attach: function (context, settings) {
-      $('.view-schedules table').addClass('table table-bordered');
-    }
-  };
-})(jQuery, Drupal, drupalSettings);
